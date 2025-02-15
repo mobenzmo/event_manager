0b7ac60568e34dbc240c73a8b402296b5cf1fe91
Mohamed <benzahiamo@gmail.com>
2025-02-15 20:13:51 +0100

    Add Auth and sign in page view

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  skip_before_action :authorize_request, only: [:index]
+
+  def index
+  end
+end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import "bootstrap";
+import "@popperjs/core";
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var triggerTabList = [].slice.call(document.querySelectorAll('.nav-link'))
+  triggerTabList.forEach(function(triggerEl) {
+    var tabTrigger = new bootstrap.Tab(triggerEl)
+    triggerEl.addEventListener('click', function(event) {
+      event.preventDefault()
+      tabTrigger.show()
+    })
+  })
+})
